bb94ff4f07e4354324948429d1c8aff3dbb01413
ahreum0408 <ahreun0408@gmail.com>
2023-10-24 19:18:50 +0900

    setting and player

---
+++ Assets/Mobile/02_Scripts/MoblieJoystick.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoblieJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler {
+    public void OnPointerDown(PointerEventData eventData) {
+        
+    }
+    public void OnPointerUp(PointerEventData eventData) {
+        
+    }
+    public void OnPointerMove(PointerEventData eventData) {
+        
+    }
+
+}
